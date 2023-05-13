0b34638d9404f6a329ff84d2109a127789842aba
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-05-13 16:56:09 -0300

    read one product e all products routes

--- shopper-test-back/routes/product.routes.js
+++ shopper-test-back/routes/product.routes.js
@@ -3,9 +3,23 @@ import Product from "../model/product.model.js"
 
 const productRouter = express.Router()
 
+
+// Read one product
+productRouter.get("/:code", async (req, res) => {
+  try {
+    const product = await Product.findOne({attributes: ['code', 'name', 'cost_price', 'sales_price'], where: {
+      code: req.params.code
+    }})
+    return res.status(200).json(product);
+  } catch (err) {
+    console.error('Não foi possível obter os produtos:', err);
+  }
+})
+
+// Read all products
 productRouter.get("/products", async (req, res) => {
   try {
-    const allProducts = await Product.findAll();
+    const allProducts = await Product.findAll({attributes: ['code', 'name', 'cost_price', 'sales_price']});
     console.log(allProducts);
     return res.status(200).json(allProducts);
   } catch (err) {

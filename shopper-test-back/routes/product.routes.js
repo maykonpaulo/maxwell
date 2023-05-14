39de097fc4fa81936cb31f9070942354e1a709d5
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-05-13 21:23:29 -0300

    if update a product update a pack with tha same product

--- shopper-test-back/routes/product.routes.js
+++ shopper-test-back/routes/product.routes.js
@@ -31,6 +31,8 @@ productRouter.get("/:code", async (req, res) => {
 productRouter.put("/:code", async (req, res) => {
   try {
     const allPacks = await Pack.findAll()
+
+
     const updatedProduct = await Product.update(
       {
         name: req.body.name,
@@ -41,21 +43,66 @@ productRouter.put("/:code", async (req, res) => {
     );
 
     // If the product is in a pack, change the pack price
-    const product = await Product.findOne({ where: {
-      code: req.params.code
-    }})
-    allPacks.forEach(async (currentKey) => {
-if (currentKey.dataValues.product_id == req.params.code) {
-  await Product.update(
-    {
-      name: req.body.name,
-        cost_price: req.body.cost_price,
-        sales_price: currentKey.dataValues.qty * product.dataValues.sales_price
-    },
-     { where: { code: currentKey.dataValues.pack_id }, }
-  )
+    const allProducts = await Product.findAll()
+
+let packProducts = []
+let packQty = []
+let productPrice = []
+let packPrice = 0
+let packId = 0
+
+    allPacks.forEach(async (currentPack) => {
+
+if (currentPack.dataValues.product_id == req.params.code) {
+  allPacks.forEach(async (current) => {
+    if (current.dataValues.pack_id == currentPack.dataValues.pack_id) {
+      packProducts.push(current.dataValues.product_id)
+      packQty.push(current.dataValues.qty)
+      packId = current.dataValues.pack_id
+
+    }
+  })
 }
+
+
+
     })
+
+allProducts.forEach(async (currentProduct) => {
+packProducts.forEach(async (current) => {
+  if (currentProduct.dataValues.code == current) {
+    productPrice.push(currentProduct.dataValues.sales_price)
+    console.log(productPrice)
+  }
+})
+})
+
+productPrice.map((currentPrice, index) => {
+  packQty.map((currentQty, i) => {
+    if (i === index) {
+      packPrice += currentPrice * currentQty
+      console.log(productPrice)
+
+    }
+  })
+})
+
+    await Product.update(
+      {
+        name: req.body.name,
+          cost_price: req.body.cost_price,
+          sales_price: packPrice
+      },
+       { where: { code: packId }, }
+    )
+
+    console.log(packPrice)
+
+// Update a pack price and update the product price
+// if (product.dataValues.code.length >= 4) {
+//   console.log(product)
+// }
+
     return res.status(200).json(updatedProduct);
   } catch (err) {
     console.error('Não foi possível atualizar o produto:', err);
